13ba0dd11619d4258f9794f4872a77e57d9c208d
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-19 11:05:15 +0200

    KA-118 - Refactor orderedIdsReducer to typescript

--- onboarding-task/src/reducers/items/orderedIdsReducer.tsx
+++ onboarding-task/src/reducers/items/orderedIdsReducer.tsx
@@ -3,9 +3,10 @@ import { List as ImmutableList } from 'immutable';
 import {
   ITEM_ADDED,
   ITEM_DELETED,
-} from '../../actions/actionTypes.tsx';
+} from '../../actions/actionTypes';
+import { IAction } from '../../actions/actionCreators';
 
-export function orderedIdsReducer(orderedIds = ImmutableList(), action) {
+export function orderedIdsReducer(orderedIds = ImmutableList(), action: IAction) {
   switch (action.type) {
     case ITEM_DELETED:
       return orderedIds.filter(x => x !== action.payload.id);
