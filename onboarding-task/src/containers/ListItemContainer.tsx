5c53f6f65d8b1caa55c538ffa4c44d1416c94585
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-19 10:48:28 +0200

    KA-118 - Refactor ListItemContainer to typescript

--- onboarding-task/src/containers/ListItemContainer.tsx
+++ onboarding-task/src/containers/ListItemContainer.tsx
@@ -1,4 +1,4 @@
-import { connect } from 'react-redux';
+import { connect, Dispatch } from 'react-redux';
 
 import {
   saveItem,
@@ -6,21 +6,30 @@ import {
   startEditingItem,
   stopEditingItem,
   updateItemText,
-} from '../actions/actionCreators.tsx';
+} from '../actions/actionCreators';
 import { ListItem } from '../components/ListItem';
-import { memoizedItemViewModelConstructor } from '../models/ItemViewModel.tsx';
+import { memoizedItemViewModelConstructor } from '../models/ItemViewModel';
+import { IState } from '../reducers/appReducer';
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+
+interface IListItemContainerProps {
+  id: string;
+  text: string;
+  index: number;
+}
+
+// TODO dispatch type
+const mapDispatchToProps = (dispatch: Dispatch<any>, ownProps: IListItemContainerProps) => {
   return {
-    onSave: (text) => dispatch(saveItem(ownProps.id, text)),
+    onSave: (text: string) => dispatch(saveItem(ownProps.id, text)),
     onDelete: () => dispatch(deleteItem(ownProps.id)),
-    onUpdate: (text) => dispatch(updateItemText(ownProps.id, text)),
+    onUpdate: (text: string) => dispatch(updateItemText(ownProps.id, text)),
     onCancel: () => dispatch(stopEditingItem(ownProps.id)),
     onEdit: () => dispatch(startEditingItem(ownProps.id)),
   };
 };
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state: IState, ownProps: IListItemContainerProps) => {
   const item = state.items.itemsByIds.get(ownProps.id);
   return {
     item: memoizedItemViewModelConstructor(item.id, item.textShown, ownProps.index, item.isEditing),
