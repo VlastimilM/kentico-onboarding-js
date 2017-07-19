f9a2a5f65cc76574ac94cee48f5052392e47022c
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-19 09:35:12 +0200

    KA-118 - Refactor Item to typescript

--- onboarding-task/src/models/Item.tsx
+++ onboarding-task/src/models/Item.tsx
@@ -1,8 +1,32 @@
 import { Record } from 'immutable';
 
-export const Item = new Record({
+interface IItem {
+  readonly id: string;
+  readonly textSaved: string;
+  readonly textShown: string;
+  readonly isEditing: boolean;
+}
+
+const defaultItem: IItem = {
   id: '',
   textSaved: '',
   textShown: '',
   isEditing: false,
-});
+};
+
+class Item extends Record(defaultItem) implements IItem {
+  readonly id: string;
+  readonly textSaved: string;
+  readonly textShown: string;
+  readonly isEditing: boolean;
+
+  constructor(params?: Partial<IItem>) {
+    params ? super(params) : super();
+  }
+
+  with(values: Partial<IItem>): Item {
+    return this.merge(values) as this;
+  }
+}
+
+export {Item};
