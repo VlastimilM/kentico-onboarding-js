d6bb5bf23a0af60538656241e4b02b989b673824
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-24 07:26:05 +0200

    KA-118 - Create interfaces in ListItemContainer

--- onboarding-task/src/containers/ListItemContainer.tsx
+++ onboarding-task/src/containers/ListItemContainer.tsx
@@ -9,17 +9,30 @@ import {
 } from '../actions/actionCreators';
 
 import { ListItem } from '../components/ListItem';
-import { memoizedItemViewModelConstructor } from '../models/ItemViewModel';
+import { memoizedItemViewModelConstructor, IItemViewModel } from '../models/ItemViewModel';
 import { IStore } from '../reducers/appReducer';
 
-
-interface IListItemContainerProps {
+export interface IListItemContainerProps {
   id: string;
   index: number;
 }
 
+// TODO rename
+export interface IListItemState {
+  item: IItemViewModel;
+}
+
+// TODO rename
+export interface IListItemDispatch {
+  onSave: (text: string) => void;
+  onDelete: () => void;
+  onUpdate: (text: string) => void;
+  onCancel: () => void;
+  onEdit: () => void;
+}
+
 // TODO dispatch type
-const mapDispatchToProps = (dispatch: Dispatch<any>, ownProps: IListItemContainerProps) => {
+const mapDispatchToProps = (dispatch: Dispatch<any>, ownProps: IListItemContainerProps): IListItemDispatch => {
   return {
     onSave: (text: string) => dispatch(saveItem(ownProps.id, text)),
     onDelete: () => dispatch(deleteItem(ownProps.id)),
@@ -29,13 +42,13 @@ const mapDispatchToProps = (dispatch: Dispatch<any>, ownProps: IListItemContaine
   };
 };
 
-const mapStateToProps = (state: IStore, ownProps: IListItemContainerProps) => {
+const mapStateToProps = (state: IStore, ownProps: IListItemContainerProps): IListItemState => {
   const item = state.items.itemsByIds.get(ownProps.id);
   return {
     item: memoizedItemViewModelConstructor(item.id, item.textShown, ownProps.index, item.isEditing),
   };
 };
 
-const listItemContainer = connect(mapStateToProps, mapDispatchToProps)(ListItem);
+const listItemContainer: React.ComponentClass<IListItemContainerProps> = connect(mapStateToProps, mapDispatchToProps)(ListItem);
 
 export { listItemContainer as ListItem };
