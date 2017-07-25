528e4cb30466fa5ab1063d1f28407e4fd373f60c
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-25 09:45:48 +0200

    KA-118 - Add type prop to button elements

--- onboarding-task/src/components/EditedListItem.tsx
+++ onboarding-task/src/components/EditedListItem.tsx
@@ -41,8 +41,8 @@ export class EditedListItem extends React.PureComponent<IEditedListItemDataProps
           <span>{this.props.item.index + 1}. </span>
           <input className="form-control" value={this.props.item.text} onChange={this._onChange} />
           <button className="btn btn-primary" type="submit" >Save</button>
-          <button className="btn btn-default" onClick={this.props.onCancel}>Cancel</button>
-          <button className="btn btn-danger" onClick={this.props.onDelete}>Delete</button>
+          <button className="btn btn-default" type="button" onClick={this.props.onCancel}>Cancel</button>
+          <button className="btn btn-danger" type="button" onClick={this.props.onDelete}>Delete</button>
         </form>
       </div>
     );
