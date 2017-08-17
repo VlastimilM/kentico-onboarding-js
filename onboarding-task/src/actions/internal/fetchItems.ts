93c8ce1d3add4d05d8ea6742048dcb892df64be4
Vlastimil Martinek <VlastimilM@kentico.com>
2017-08-17 07:32:52 +0200

    KA-163 - Add fetchItemsFactory, postItemsFactory

--- onboarding-task/src/actions/internal/fetchItems.ts
+++ onboarding-task/src/actions/internal/fetchItems.ts
@@ -2,22 +2,23 @@ import { MAIN_ROUTE } from '../../constants/routes';
 import { requestItems, receiveItems, failItemsFetch } from '../actionCreators';
 import { IAction } from '../IAction';
 
+const fetchItemsFactory = (fetchFunction: (route: string, options: Object) => Promise<any>) =>
+  (): any => {
+    return (dispatch: Dispatch): Promise<IAction> => {
+      dispatch(requestItems());
 
-// TODO DI
-export const fetchItems = (): any => {
-  return (dispatch: Dispatch): Promise<IAction> => {
-    dispatch(requestItems());
-
-    let options = { method: 'GET' };
-    return fetch(MAIN_ROUTE, options)
-      .then(
-        (response: any) => response.json(),
-        (error: any) => {
-          throw new Error(error);
-        })
-      .then(
-        (json: any) => dispatch(receiveItems(json)),
-        () => dispatch(failItemsFetch())
-      );
+      let options = { method: 'GET' };
+      return fetchFunction(MAIN_ROUTE, options)
+        .then(
+          (response: any) => response.json(),
+          (error: any) => {
+            throw new Error(error);
+          })
+        .then(
+          (json: any) => dispatch(receiveItems(json)),
+          () => dispatch(failItemsFetch())
+        );
+    };
   };
-};
+
+export const fetchItems = fetchItemsFactory(fetch);
--- onboarding-task/src/actions/internal/postItem.ts
+++ onboarding-task/src/actions/internal/postItem.ts
@@ -1,24 +1,25 @@
 import { failPostItem, receiveItem, requestPostItem, addItem } from '../actionCreators';
 
-// TODO DI
-export const postItem = (text: string): any => {
-  return (dispatch: Dispatch) => {
-    let header = new Headers({
-      'Content-Type': 'application/json',
-    });
-    dispatch(requestPostItem());
-    console.log(addItem(text));
-    dispatch(addItem(text));
+const postItemFactory = (fetchFunction: (route: string, options: Object) => Promise<any>) =>
+  // TODO return type
+  (text: string): any => {
+    return (dispatch: Dispatch) => {
+      let header = new Headers({
+        'Content-Type': 'application/json',
+      });
+      dispatch(requestPostItem());
+      dispatch(addItem(text));
 
-    fetch('/api/v1/ListItems/', {
-      method: 'POST',
-      headers: header,
-      body: JSON.stringify({ text })
-    })
-      .then((response: any) => response.json(), (error: any) => {
-        throw new Error(error);
+      fetchFunction('/api/v1/ListItems/', {
+        method: 'POST',
+        headers: header,
+        body: JSON.stringify({ text })
       })
-      .then((json: any) => dispatch(receiveItem(json)), () => dispatch(failPostItem()));
+        .then((response: any) => response.json(), (error: any) => {
+          throw new Error(error);
+        })
+        .then((json: any) => dispatch(receiveItem(json)), () => dispatch(failPostItem()));
+    };
   };
-};
 
+export const postItem = postItemFactory(fetch);
