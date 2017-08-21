c59ea26a6bf7fd8cfd00acc0337a81359bbc3948
Vlastimil Martinek <VlastimilM@kentico.com>
2017-08-21 08:38:30 +0200

    KA-163 - Add fetch items actions tests

--- onboarding-task/test/actions/actionCreators.test.js
+++ onboarding-task/test/actions/actionCreators.test.js
@@ -1,11 +1,17 @@
+import Immutable from 'immutable';
+
 import {
   saveItem,
   deleteItem,
   startEditingItem,
   stopEditingItem,
   updateItemText,
+  requestItems,
+  receiveItems,
 } from '../../src/actions/actionCreators.ts';
+import { Item } from '../../src/models/Item';
 import { addItemFactory } from '../../src/actions/addItemFactory.ts';
+import { failItemsFetchFactory } from '../../src/actions/failItemsFetchFactory.ts';
 import {
   ITEM_ADDED,
   ITEM_SAVED,
@@ -13,9 +19,14 @@ import {
   START_EDITING_ITEM,
   STOP_EDITING_ITEM,
   UPDATE_ITEM_TEXT,
+  FETCH_ITEMS_REQUEST,
+  FETCH_ITEMS_FAILURE,
+  FETCH_ITEMS_SUCCESS,
 } from '../../src/actions/actionTypes.ts';
 
 describe('Action Creators', () => {
+  const failItemsFetch = failItemsFetchFactory(() => '5');
+
   it('create ITEM_ADDED action correctly', () => {
     expect(addItemFactory(() => '5')('testText')).toEqual({
       type: ITEM_ADDED,
@@ -69,6 +80,55 @@ describe('Action Creators', () => {
       payload: {
         id: '5',
         text: 'newText',
+      },
+    });
+  });
+
+  it('create FETCH_ITEMS_REQUEST action correctly', () => {
+    expect(requestItems()).toEqual({
+      type: FETCH_ITEMS_REQUEST,
+    });
+  });
+
+  it('create FETCH_ITEMS_FAILURE action correctly', () => {
+    expect(failItemsFetch()).toEqual({
+      type: FETCH_ITEMS_FAILURE,
+      payload: {
+        errorId: '5',
+      },
+    });
+  });
+
+  it('create FETCH_ITEMS_REQUEST action correctly with no items', () => {
+    const items = [];
+    expect(receiveItems(items)).toEqual({
+      type: FETCH_ITEMS_SUCCESS,
+      payload: {
+        items: Immutable.Map(),
+      },
+    });
+  });
+
+  // TODO multiple items
+  it('create FETCH_ITEMS_REQUEST action correctly with item', () => {
+    const itemText = 'awawa';
+    const itemId = '5';
+    const firstItem = {
+      id: itemId,
+      text: itemText,
+    };
+    const items = [firstItem];
+    const expectedItems = Immutable.Map()
+      .set(itemId, new Item().withValues({
+        id: itemId,
+        textSaved: itemText,
+        textShown: itemText,
+        isEditing: false,
+      }));
+    expect(receiveItems(items)).toEqual({
+      type: FETCH_ITEMS_SUCCESS,
+      payload: {
+        items: expectedItems,
       },
     });
   });
