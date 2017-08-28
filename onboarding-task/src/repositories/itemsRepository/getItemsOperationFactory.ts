9b74f33d5f63f685fc9ab6669185779af3916315
Vlastimil Martinek <VlastimilM@kentico.com>
2017-08-28 13:13:40 +0200

    KA-161 - Add postItemOperation to getItemsOperationFactory.ts

--- onboarding-task/src/repositories/itemsRepository/getItemsOperationFactory.ts
+++ onboarding-task/src/repositories/itemsRepository/getItemsOperationFactory.ts
@@ -15,3 +15,25 @@ export const getItemsOperationFactory = (fetch: FetchFunction) =>
       })
       .then((response) => handleFetch(response));
   };
+
+export const postItemOperationFactory = (fetch: FetchFunction) =>
+  (text: string): Promise<ServerItem> => {
+    const header = new Headers({
+      'Content-Type': 'application/json',
+    });
+    const fetchOptions = {
+      method: 'POST',
+      headers: header,
+      body: JSON.stringify({ text })
+    };
+
+    return fetch(MAIN_ROUTE, fetchOptions)
+      .catch(() => {
+        throw new Error('Failed to post item. You are offline.');
+      })
+      .then((response) => handleFetch(response));
+  };
+
+export const getItemsOperation = getItemsOperationFactory(fetch);
+
+export const postItemOperation = postItemOperationFactory(fetch);
