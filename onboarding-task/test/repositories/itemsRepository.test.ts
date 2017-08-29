3e8f77e5c0b25edc272b618272742342af56064c
Vlastimil Martinek <VlastimilM@kentico.com>
2017-08-29 09:55:02 +0200

    KA-163 - Add repository tests, perform fatality on task 5

--- onboarding-task/test/repositories/itemsRepository.test.ts
+++ onboarding-task/test/repositories/itemsRepository.test.ts
@@ -19,12 +19,17 @@ describe('items repository', () => {
   const receivedItems: Array<ServerItem> = [postedItem];
 
   const fetchResponse: IResponse = { ok: true, json: () => Promise.resolve(receivedItems) };
+  const getItemsError = new Error('Failed to fetch items. You are offline.');
   const successfulGetItemsFetch = (_route: string, _options: IHttpRequestOptions): Promise<IResponse> =>
     Promise.resolve(fetchResponse);
+  const failedGetItemsOperation = () => Promise.reject(getItemsError);
 
   const postResponse: IResponse = { ok: true, json: () => Promise.resolve(postedItem) };
+  const postItemError = new Error('Failed to post item. You are offline.');
   const successfulPostItemFetch = (_route: string, _options: IHttpRequestOptions): Promise<IResponse> =>
     Promise.resolve(postResponse);
+  const failedPostItemOperation = (_text: string) => Promise.reject(postItemError);
+
 
   it('calls fetch in fetchItems with MAIN_ROUTE argument', () => {
     const fetchMock = jest.fn(successfulGetItemsFetch);
@@ -51,5 +56,33 @@ describe('items repository', () => {
         expect(fetchMock.mock.calls[0][1].method).toEqual('POST');
         expect(fetchMock.mock.calls[0][1].body).toEqual(JSON.stringify({ text: postItemText }));
       });
+  });
+
+  it('returns correct data on successful postItem', () => {
+    const postItemOperation = postItemOperationFactory(successfulPostItemFetch);
+
+    return (postItemOperation(postItemText)
+      .then((data) => expect(data).toEqual(postedItem)));
+  });
+
+  it('returns correct error on failed postItem', () => {
+    const postItemOperation = postItemOperationFactory(failedPostItemOperation);
+
+    return (postItemOperation(postItemText)
+      .catch((error) => expect(error).toEqual(postItemError)));
+  });
+
+  it('returns correct data on successful getItems', () => {
+    const getItemsOperation = getItemsOperationFactory(successfulGetItemsFetch);
+
+    return (getItemsOperation()
+      .then((data) => expect(data).toEqual(receivedItems)));
+  });
+
+  it('returns correct error on failed getItems', () => {
+    const getItemsOperation = getItemsOperationFactory(failedGetItemsOperation);
+
+    return (getItemsOperation()
+      .catch((error) => expect(error).toEqual(getItemsError)));
   });
 });
